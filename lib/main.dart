62bc921e5ba23e7d2b6bdf6d8191a7a749d09bc7
wallyngson <wallyngson.guedes@ccc.ufcg.edu.br>
2019-09-17 23:00:28 -0300

    configuration-firebase-windows

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+ 
+void main() {
+  Firestore.instance.collection('teste');
+
+  runApp(MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
